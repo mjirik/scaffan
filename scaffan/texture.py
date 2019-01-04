fix test with energy computation

scaffan/texture.py
@@ -11,11 +11,19 @@ import scipy.ndimage
 import matplotlib.pyplot as plt
 
 
-def block_processing(image, fcn, tile_size, return_centers=False):
-    if return_centers:
-        tile_size2 = [int(tile_size[0] / 2), int(tile_size[1] / 2)]
-        centers = []
-    output = np.asarray(image, dtype=np.int8)
+def tile_centers(image_shape, tile_size):
+    tile_size2 = [int(tile_size[0] / 2), int(tile_size[1] / 2)]
+    centers = []
+    for x0 in range(0, image_shape[0], tile_size[0]):
+        for x1 in range(0, image_shape[1], tile_size[1]):
+            centers.append([x0 + tile_size2[0], x1 + tile_size2[1]])
+    return centers
+
+def tiles_processing(image, fcn, tile_size, fcn_output_n=None):
+    shape = image.shape
+    if fcn_output_n is not None:
+        shape.append(fcn_output_n)
+    output = np.zeros(shape, dtype=np.int8)
     for x0 in range(0, image.shape[0], tile_size[0]):
         for x1 in range(0, image.shape[1], tile_size[1]):
             sl = [
@@ -25,11 +33,6 @@ def block_processing(image, fcn, tile_size, return_centers=False):
             img = image[sl]
             output [sl] = fcn(img)
 
-            if return_centers:
-                centers.append([x0 + tile_size2[0], x1 + tile_size2[1]])
-
-    if return_centers:
-        return output, centers
     else:
         return output
 
@@ -154,16 +157,14 @@ class TextureSegmentation:
         test_image = view.get_region_image(as_gray=True)
 
         tile_fcn = lambda img: get_feature_and_predict(img, self.feature_function, self.classifier)
-        out = block_processing(test_image, tile_fcn, tile_size=self.tile_size, return_centers=show)
+        seg = tiles_processing(test_image, tile_fcn, tile_size=self.tile_size)
 
         if show:
-            seg, centers = out
+            centers = tile_centers(test_image.shape, tile_size=self.tile_size)
             import skimage.color
             plt.imshow(skimage.color.label2rgb(seg, test_image))
             x, y = list(zip(*centers))
             plt.plot(x, y, "xy")
             # view.plot_points()
-        else:
-            seg = out
         return seg
 

tests/texture_test.py
@@ -127,11 +127,11 @@ class TextureTest(unittest.TestCase):
         annotation_ids = anim.select_annotations_by_title("test2")
         view_test = anim.get_views(annotation_ids, level=level)[0]
         test_image = view_test.get_region_image(as_gray=True)
-        target, data = list(zip(refs))
+        target, data = list(zip(*refs))
         cls = salbp.KLDClassifier()
         cls.fit(data, target)
         tile_fnc = lambda tile: satex.get_feature_and_predict(tile, salbp.lbp_fv, cls)
-        seg = satex.block_processing(test_image, salbp.lbp_fv, cls, tile_size=size)
+        seg = satex.tiles_processing(test_image, tile_fnc, tile_size=size)
         plt.figure()
         plt.imshow(test_image)
         plt.contour(seg)
@@ -243,20 +243,22 @@ class TextureTest(unittest.TestCase):
         # texseg.add_training_data(anim, "obj3", 3)
 
         views = anim.get_views_by_title("test1", level=texseg.level)
-        seg = texseg.predict(views[0], show=False, function=texture_energy)
+        energy = satex.tiles_processing(views[0].get_region_image(as_gray=True),
+                                        fcn=texture_energy, tile_size=texseg.tile_size)
+        # seg = texseg.predict(views[0], show=False, function=texture_energy)
         plt.figure()
         plt.subplot(121)
         img = views[0].get_region_image(as_gray=True)
         plt.imshow(img, cmap='gray')
         plt.colorbar()
         plt.subplot(122)
-        plt.imshow(seg)
+        plt.imshow(energy)
         plt.colorbar()
         plt.savefig("glcm_energy.png")
         # plt.show()
 
 
-def texture_energy(refs, img):
+def texture_energy(img):
     import skimage.feature.texture
     P = skimage.feature.greycomatrix((img * 31).astype(np.uint8), [1], [0, np.pi/2], levels=32, symmetric=True, normed=True)
     en = skimage.feature.texture.greycoprops(P, prop="energy")