21ae9fc3cff67b979a09f9e101013c55fb14986a
Miroslav Jirik <miroslav.jirik@gmail.com>
2019-01-10 11:32:12 +0100

    removed commented code

--- tests/parse_annotation_test.py
+++ tests/parse_annotation_test.py
@@ -30,37 +30,6 @@ skip_on_local = False
 
 
 class ParseAnnotationTest(unittest.TestCase):
-
-    # def test_relabel_organ_segmentation(self):
-    #     import lisa.organ_segmentation
-    #     import io3d
-    #     # datap = io3d.datasets.generate_abdominal()
-    #     datap = io3d.datasets.generate_synthetic_liver(return_dataplus=True)
-    #     slab = datap["slab"]
-    #     oseg = lisa.organ_segmentation.OrganSegmentation()
-    #     oseg.import_dataplus(datap)
-    #     self.assertGreater(np.sum(oseg.select_label("porta")), 0, "Generated porta should be bigger than 0 voxels")
-    #     oseg.segmentation_relabel("porta", "liver")
-    #
-    #     self.assertEqual(np.sum(oseg.select_label("porta")), 0)
-    #     self.assertGreater(np.sum(oseg.select_label("liver")), 100)
-    #
-    # def test_relabel_organ_segmentation_from_multiple_labels(self):
-    #     import lisa.organ_segmentation
-    #     import io3d
-    #     # datap = io3d.datasets.generate_abdominal()
-    #     datap = io3d.datasets.generate_synthetic_liver(return_dataplus=True)
-    #     slab = datap["slab"]
-    #     slab["tumor"] = 3
-    #     datap["segmentation"][5:10, 5:10, 5:10] = slab["tumor"]
-    #     oseg = lisa.organ_segmentation.OrganSegmentation()
-    #     oseg.import_dataplus(datap)
-    #     self.assertGreater(np.sum(oseg.select_label("porta")), 0, "Generated porta should be bigger than 0 voxels")
-    #     oseg.segmentation_relabel(["porta", "tumor"], "liver")
-    #
-    #     self.assertEqual(np.sum(oseg.select_label("porta")), 0)
-    #     self.assertEqual(np.sum(oseg.select_label("tumor")), 0)
-    #     self.assertGreater(np.sum(oseg.select_label("liver")), 100)
     @unittest.skipIf(
         platform.system() == "Windows",
         "On windows there is problem with openslides import. Test works standalone but not together with the others.",
