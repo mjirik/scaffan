653b0e2f5f43a4f7365150cfa085da0faaf94f10
Miroslav Jirik <miroslav.jirik@gmail.com>
2019-01-27 08:28:19 +0100

    fixed windows typing in linux script

--- conda-recipe/build.sh
+++ conda-recipe/build.sh
@@ -10,11 +10,13 @@ echo "scaffan build.sh" > ~/scaffan.txt
 pwd
 ls
 
-echo "prefix"
-echo %PREFIX%
-ls %PREFIX%
-echo "recipe dir"
-echo %RECIPE_DIR%
-ls %RECIPE_DIR%
+echo "prefix $PREFIX"
+ls $PREFIX
+echo "recipe dir $RECIPE_DIR"
+ls $RECIPE_DIR
+echo "prefix $PREFIX" >> ~/scaffan.txt
+ls $PREFIX >> ~/scaffan.txt
+echo "recipe dir $RECIPE_DIR" >> ~/scaffan.txt
+ls $RECIPE_DIR >> ~/scaffan.txt
 # rm -rf examples
 $PYTHON setup.py install
