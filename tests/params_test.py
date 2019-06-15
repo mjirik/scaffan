8c2c6062e2ac61e3a8b66187c016a8b58eb725ea
Miroslav Jirik <miroslav.jirik@gmail.com>
2019-06-15 13:18:30 +0200

    added pyqtgraph support

---
+++ tests/params_test.py
@@ -0,0 +1,36 @@
+#! /usr/bin/python
+# -*- coding: utf-8 -*-
+
+from loguru import logger
+import unittest
+import os
+import io3d
+from pathlib import Path
+import shutil
+import pandas as pd
+
+
+# import openslide
+import scaffan
+import scaffan.algorithm
+import pyqtgraph.parametertree
+
+import scaffan.dilipg
+
+
+
+
+class ParameterTest(unittest.TestCase):
+
+    skip_on_local = False
+    # skip_on_local = True
+    # @unittest.skipIf(os.environ.get("TRAVIS", skip_on_local), "Skip on Travis-CI")
+    def test_save_report_excel(self):
+        mainapp = scaffan.algorithm.Scaffan()
+        p = mainapp.parameters
+        # mainapp = scaffan.Scaffan()
+        dct = scaffan.dilipg.params_and_values(p)
+
+        print(dct)
+        print(p)
+        pass
