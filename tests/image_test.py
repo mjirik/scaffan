9c194c5a60eb3d9b1f9bbec68e015f1c55385271
Miroslav Jirik <miroslav.jirik@gmail.com>
2019-07-06 10:31:46 +0200

    alpha

--- tests/image_test.py
+++ tests/image_test.py
@@ -162,9 +162,9 @@ class ImageAnnotationTest(unittest.TestCase):
         # plt.show()
 
         merged = view1.insert_image_from_view(view2, image1, image2)
-        logger.debug(f"image1 dtype: {image1.dtype}, shape: {image1.shape}, min max: [{np.min(image1)}, {np.max(image1)}], mean: {np.mean(image1)}")
-        logger.debug(f"image2 dtype: {image2.dtype}, shape: {image2.shape}, min max: [{np.min(image2)}, {np.max(image2)}], mean: {np.mean(image2)}")
-        logger.debug(f"merged dtype: {merged.dtype}, shape: {merged.shape}, min max: [{np.min(merged)}, {np.max(merged)}], mean: {np.mean(merged)}")
+        logger.debug(f"image1 dtype: {image1.dtype}, shape: {image1.shape}, min max: [{np.min(image1[:,:,:3])}, {np.max(image1[:,:,:3])}], mean: {np.mean(image1[:,:,:3])}, min max alpha: [{np.min(image1[:,:,:3])}, {np.max(image1[:,:,:3])}], mean: {np.mean(image1[:,:,:3])}")
+        logger.debug(f"image2 dtype: {image2.dtype}, shape: {image2.shape}, min max: [{np.min(image2[:,:,:3])}, {np.max(image2[:,:,:3])}], mean: {np.mean(image2[:,:,:3])}, min max alpha: [{np.min(image2[:,:,:3])}, {np.max(image2[:,:,:3])}], mean: {np.mean(image2[:,:,:3])}")
+        logger.debug(f"merged dtype: {merged.dtype}, shape: {merged.shape}, min max: [{np.min(merged[:,:,:3])}, {np.max(merged[:,:,:3])}], mean: {np.mean(merged[:,:,:3])}, min max alpha: [{np.min(merged[:,:,:3])}, {np.max(merged[:,:,:3])}], mean: {np.mean(merged[:,:,:3])}")
 
         # plt.imshow(merged)
         # plt.show()
