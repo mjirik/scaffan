fixed wrong dims in reverse resize on pixelsize_mm

scaffan/image.py
@@ -986,10 +986,11 @@ class View:
             )
             req_sz = self._requested_size_on_level_when_defined_by_pixelsize
             if req_sz is not None:
-                if not np.array_equal(im_resized.shape, req_sz):
+                req_sz = np.asarray(req_sz)
+                if not np.array_equal(im_resized.shape[:2], req_sz):
                     # Array should be the same size.
                     # Due to numerical error in alpha computation there can be small pixel error
-                    norm = np.linalg.norm(im_resized.shape[:2], req_sz)
+                    norm = np.linalg.norm(im_resized.shape[:2] - req_sz)
                     if norm > 3. :
                         logger.error(f"Requested size ({req_sz}) differ "\
                         f"from the real image size ({im_resized.shape}) a lot. Fixing by resize.")

tests/whole_scan_segmentation_unet.py
@@ -40,10 +40,16 @@ def test_unet_on_view():
     ann_ids = anim.select_annotations_by_color("#FFFF00")
     # anim.get_views(ann_ids)
     view = anim.get_view(annotation_id=ann_ids[0], size_on_level=[224, 224], pixelsize_mm=[0.01, 0.01])
+    # import matplotlib.pyplot as plt
+    # im = view.get_region_image()
+    # plt.imshow(im)
+    # plt.show()
+    # assert np.array_equal(im.shape, [224, 224, 4])
     wss_unet = scaffan.whole_slide_seg_unet.WholeSlideSegmentationUNet()
     wss_unet.init_segmentation()
     prediction = wss_unet.predict_tile(view)
-    #plt.imshow(prediction)
+    # plt.imshow(prediction)
+    # plt.show()
 
     unq = np.unique(prediction)
     assert 0 in unq, "label 0 should be in prediction"