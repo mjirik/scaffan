69e99cc07e1b1f78392ac92e5293ad2925ecbf3c
Miroslav Jirik <miroslav.jirik@gmail.com>
2020-04-07 22:29:56 +0200

    use artifical image

--- tests/image_intensity_rescale_test.py
+++ tests/image_intensity_rescale_test.py
@@ -16,7 +16,11 @@ import numpy as np
 
 def test_image_intensity_rescale():
     # img = skimage.data.astronaut()
-    img = skimage.data.chelsea()
+    # img = skimage.data.chelsea()
+    img = np.random([120,80,3])
+    img[10:30, 30:50,:] += 0.4
+    img[50:70, 10:40, 0] += 0.6
+    img = (img*255).astype(np.uint8)
 
     rescaler = image_intensity_rescale.RescaleIntensityPercentile()
     rescaler.calculate_intensity_dependent_parameters(img)
