24eda73a66c1ddccf89c08a65acaea7e0786e765
Miroslav Jiřík <miroslav.jirik@gmail.com>
2021-03-27 01:17:32 +0100

    version from git describe if possible

--- scaffan/__init__.py
+++ scaffan/__init__.py
@@ -1,5 +1,10 @@
 # import os, sys; sys.path.append(os.path.dirname(os.path.realpath(__file__)))
-__version__ = "0.29.14"
+import subprocess
+try:
+
+    __version__ = subprocess.check_output("git describe".split(' '), cwd="..").strip()
+except (subprocess.CalledProcessError, FileNotFoundError) as e:
+    __version__ = "0.29.14"
 """
 Used for scaffold analysis
 """
