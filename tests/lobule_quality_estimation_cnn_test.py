f1816074c488e0a136e066aab63e979a72b461f9
Miroslav Jirik <miroslav.jirik@gmail.com>
2021-09-02 16:00:19 +0200

    faster test

--- tests/lobule_quality_estimation_cnn_test.py
+++ tests/lobule_quality_estimation_cnn_test.py
@@ -12,16 +12,23 @@ import scaffan.lobulus
 import scaffan.lobule_quality_estimation_cnn
 import pytest
 import re
+import numpy as np
+import pdb
 
 
 def test_get_lobulus_mask_manual():
     fn = io3d.datasets.join_path(
-        "medical", "orig", "sample_data", "SCP003", "SCP003.ndpi", get_root=True
+        # "medical/orig/scaffan-analysis-czi/Zeiss-scans/01_2019_11_12__RecognizedCode_crop.czi",
+        "medical", "orig", "sample_data", "SCP003", "SCP003.ndpi",
+        get_root=True
     )
+    # logger.debug(f"fn.exists={fn.exists()}")
 
     # Get the manual segmentation
+
     anim = scaffan.image.AnnotatedImage(fn)
     anns = anim.get_annotations_by_color("#FFFF00")
+    # anns = anim.get_annotations_by_color("#FF0000")
 
     report = None
     lob_proc = scaffan.lobulus.Lobulus(report=report)
@@ -30,7 +37,12 @@ def test_get_lobulus_mask_manual():
     lob_proc.set_annotated_image_and_id(anim, annotation_id=annotation_id)
     lob_proc.run()
 
+    # make the calculation faster by using about quater of the lobule
+    sh_half = (np.asarray(lob_proc.lobulus_mask.shape) / 2).astype(int)
+    lob_proc.lobulus_mask[:sh_half[0], :sh_half[1]] = False
+
     lqe = scaffan.lobule_quality_estimation_cnn.LobuleQualityEstimationCNN()
+
 
     lqe.init(force_download_model=True) # this will test the model download
     lqe.set_input_data(
