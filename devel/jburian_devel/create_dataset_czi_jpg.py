ebaf96fce0f1b38e7c39a3d659bba67f36bf5681
janburian <jburian@students.zcu.cz>
2021-11-10 11:21:00 +0100

    Added documentation of methods, minor code changes

--- devel/jburian_devel/create_dataset_czi_jpg.py
+++ devel/jburian_devel/create_dataset_czi_jpg.py
@@ -9,6 +9,18 @@ import scaffan.image
 
 
 def export_czi_annotations_to_jpg(path_annotations, annotation_name, path_images):
+    '''
+    Creates image dataset from .czi annotations
+
+    Parameters:
+    -----------
+    path_annotations : Path
+                       path to .czi files
+    annotation_name : str
+                      wanted name of the annotations (e.g. ann, annotation, a,...)
+    path_images : str
+                  path to directory where the images will be saved
+    '''
     index = 0
     while True:
         fn_str = (
--- devel/jburian_devel/export_czi_json.py
+++ devel/jburian_devel/export_czi_json.py
@@ -12,6 +12,21 @@ from datetime import date
 
 
 def get_image_properties(dataset_directory):
+    '''
+    Returns the properties of the images (list of dictionaries) which are obligatory in COCO data format
+    For example (properties of one image):
+        image {
+            "id": int,
+            "width": int,
+            "height": int,
+            "file_name": str,
+        }
+
+    Parameters:
+    -----------
+    dataset_directory : Path
+                        directory of the image dataset
+    '''
     image_name = 0
     list_image_dictionaries = []
     image_name_id = 0
@@ -41,6 +56,23 @@ def get_image_properties(dataset_directory):
 
 
 def get_category_properties(dataset_directory, filename):
+    '''
+    Returns properties of category (list of dictionaries)
+    It can read categories from .txt file, if each category is on a single line without commas
+    For example (properties of one category):
+        category {
+            "id": int,
+            "name": str,
+            "supercategory": str,
+        }
+
+    Parameters:
+    -----------
+    dataset_directory : Path
+                        directory of the image dataset
+    filename : str
+               filename of the .txt file which is in the same file as dataset
+    '''
     list_category_dictionaries = []
     with open(str(dataset_directory) + "\\" + filename) as f:
         lines = f.readlines()
@@ -49,9 +81,9 @@ def get_category_properties(dataset_directory, filename):
     for i in range(number_lines):
         supercategory = lines[i].rstrip()
         category_dictionary = {
-            "supercategory": supercategory,
             "id": i + 1,
             "name": supercategory,
+            "supercategory": supercategory,
         }
         list_category_dictionaries.append(category_dictionary)
 
@@ -59,10 +91,43 @@ def get_category_properties(dataset_directory, filename):
 
 
 def count_polygon_area(x, y):
+    '''
+    Counts the area of an polygon
+
+    Parameters:
+    -----------
+    x : np.array(x_px_list),
+    y : np.array(y_px_list)
+    '''
     return 0.5 * np.abs(np.dot(x, np.roll(y, 1)) - np.dot(y, np.roll(x, 1)))
 
 
 def get_annotations_properties(czi_files_directory, annotation_name, pixelsize_mm):
+    '''
+    Returns the properties of the annotations (list of dictionaries)
+    One dictionary = object instance annotation
+    For example (one annotation):
+        annotation{
+            "id": int,
+            "image_id": int, (the ID of a picture where the annotation is located)
+            "category_id": int,
+            "segmentation": RLE or [polygon],
+            "area": float,
+            "bbox": [x,y,width,height],
+            "iscrowd": 0 or 1,
+        }
+
+    Parameters:
+    -----------
+    czi_files_directory : Path
+                          path to .czi files directory
+    annotation_name : Str
+                      it is supposed, that the all annotations have the same name (e.g. annotation0001, annotation0002,...)
+    pixelsize_mm : list
+                   defines pixelsize in mm (e.g. pixelsize = [[0.0003, 0.0003])
+
+    '''
+
     index = 0
     annotation_id = 1
     category_id = 1  # only one category - cells
@@ -142,51 +207,92 @@ def get_annotations_properties(czi_files_directory, annotation_name, pixelsize_m
     return list_annotation_dictionaries
 
 
-# Vytvoreni souboru .json
-import json
+def get_info_dictionary(version, description, contributor):
+    '''
+    Returns dictionary with the basic information related to COCO dataset
 
-# .json structure
-"""
-data = {    
-    "info" : info, 
-    "images": [image],
-    "categories": [],
-    "annotations": [annotation]
-    "licenses": [license]
-}
-"""
+    Parameters:
+    -----------
+    version : str
+    description: str
+    contributor : st
+    '''
 
-data = {}
-info_dictionary = {
-    "year": str(date.today().year),
-    "version": 1.0,
-    "description": "COCO dataset for scaffan",
-    "contributor": "Jan Burian",
-    "date_created": date.today().strftime("%d/%m/%Y"),
-}
-data.update({"info": info_dictionary})
+    info_dictionary = {
+        "year": str(date.today().year),
+        "version": version,
+        "description": description,
+        "contributor": contributor,
+        "date_created": date.today().strftime("%d/%m/%Y"),
+    }
+    return info_dictionary
 
-dataset_directory = Path(r"H:\COCO_dataset\images")
 
-list_image_dictionaries = get_image_properties(dataset_directory)
-data.update({"images": list_image_dictionaries})
+if __name__ == "__main__":
+    '''
+    Creating of .json file
+    '''
+    import json
 
-list_category_dictionaries = get_category_properties(
-    dataset_directory, "categories.txt"
-)  # directory and .txt file
-data.update({"categories": list_category_dictionaries})
+    '''
+    .json file structure
+    
+    data = {    
+        "info" : info, 
+        "images": [image],
+        "categories": [],
+        "annotations": [annotation]
+        "licenses": [license]
+    }
+    '''
 
-czi_files_directory = Path(r"H:\zeiss_export_json")  # path to .czi files directory
-annotation_name = "annotation"
+    #Directory of the image dataset
+    dataset_directory = Path(r"H:\COCO_dataset\images")
 
-pixelsize_mm = [0.0003, 0.0003]
-list_annotation_dictionaries = get_annotations_properties(
-    czi_files_directory, annotation_name, pixelsize_mm
-)
-data.update({"annotations": list_annotation_dictionaries})
+    #Directory of the .czi files
+    czi_files_directory = Path(r"H:\zeiss_export_json")  # path to .czi files directory
 
-# Creating COCO format
-path_json = "H:\\COCO_dataset"  # path to directory, where the images will be exported
-# Creating .json file
-with open(path_json + "\\" + "trainval.json", "w", encoding="utf-8") as f:
-    json.dump(data, f, ensure_ascii=False, indent=4)
+    data = {}
+
+    '''
+    Info
+    '''
+    version = "1.0"
+    description = "COCO dataset for scaffan"
+    contributor = "Jan Burian"
+
+    info_dictionary = get_info_dictionary(version, description, contributor)
+    data.update({"info": info_dictionary})
+
+    '''
+    Images
+    '''
+    list_image_dictionaries = get_image_properties(dataset_directory)
+    data.update({"images": list_image_dictionaries})
+
+    '''
+    Categories
+    '''
+    list_category_dictionaries = get_category_properties(
+        dataset_directory, "categories.txt"
+    )  # directory and .txt file
+    data.update({"categories": list_category_dictionaries})
+
+    '''
+    Annotations
+    '''
+    annotation_name = "annotation"
+    pixelsize_mm = [0.0003, 0.0003]
+    list_annotation_dictionaries = get_annotations_properties(
+        czi_files_directory, annotation_name, pixelsize_mm
+    )
+    data.update({"annotations": list_annotation_dictionaries})
+
+
+    '''
+    COCO format
+    '''
+    path_json = "H:\\COCO_dataset"  # path to directory, where the .json file will be exported
+    # Creating .json file
+    with open(path_json + "\\" + "trainval.json", "w", encoding="utf-8") as f:
+        json.dump(data, f, ensure_ascii=False, indent=4)
