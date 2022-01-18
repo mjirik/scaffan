ed33622194348cdfe2811cb0d052212b34a231b1
autoblack <mjirik@users.noreply.github.com>
2022-01-18 10:10:51 +0000

    fixup: Format Python code with Black

--- devel/jburian_devel/export_czi_json.py
+++ devel/jburian_devel/export_czi_json.py
@@ -250,7 +250,9 @@ if __name__ == "__main__":
     dataset_directory = Path(r"H:\BP\COCO_dataset_validate\images")
 
     # Directory of the .czi files
-    czi_files_directory = Path(r"H:\BP\data\czi_files_validate")  # path to .czi files directory
+    czi_files_directory = Path(
+        r"H:\BP\data\czi_files_validate"
+    )  # path to .czi files directory
     czi_files_directory = Path(
         r"H:\BP\data\czi_files_train"
     )  # path to .czi files directory
@@ -294,9 +296,7 @@ if __name__ == "__main__":
     """
     COCO format
     """
-    path_json = (
-        "H:\\BP\\COCO_dataset_validate"  # path to directory, where the .json file will be saved
-    )
+    path_json = "H:\\BP\\COCO_dataset_validate"  # path to directory, where the .json file will be saved
     path_json = "H:\\BP\\COCO_dataset_train"  # path to directory, where the .json file will be exported
     # Creating .json file
     with open(path_json + "\\" + "trainval.json", "w", encoding="utf-8") as f:
