c2f787e6bec300cd980bbbd9722b67f1ac948bc0
janburian <jburian@students.zcu.cz>
2022-01-18 11:15:16 +0100

    Changed paths

--- devel/jburian_devel/export_czi_json.py
+++ devel/jburian_devel/export_czi_json.py
@@ -247,14 +247,10 @@ if __name__ == "__main__":
     """
 
     # Directory of the image dataset
-    dataset_directory = Path(r"H:\BP\COCO_dataset_validate\images")
+    dataset_directory = Path(r"H:\BP\COCO_dataset_train\images")
 
     # Directory of the .czi files
-    czi_files_directory = Path(r"H:\BP\data\czi_files_validate")  # path to .czi files directory
-    czi_files_directory = Path(
-        r"H:\BP\data\czi_files_train"
-    )  # path to .czi files directory
-
+    czi_files_directory = Path(r"H:\BP\data\czi_files_train")  # path to .czi files directory
     data = {}
 
     """
@@ -295,9 +291,8 @@ if __name__ == "__main__":
     COCO format
     """
     path_json = (
-        "H:\\BP\\COCO_dataset_validate"  # path to directory, where the .json file will be saved
+        "H:\\BP\\COCO_dataset_train"  # path to directory, where the .json file will be saved
     )
-    path_json = "H:\\BP\\COCO_dataset_train"  # path to directory, where the .json file will be exported
     # Creating .json file
     with open(path_json + "\\" + "trainval.json", "w", encoding="utf-8") as f:
         json.dump(data, f, ensure_ascii=False, indent=4)
