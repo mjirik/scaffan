c813a7f25dbed094ad63d690f5eac225301351a3
janburian <jburian@students.zcu.cz>
2022-03-10 12:05:42 +0100

    Some attempts of uploading files

--- devel/jburian_devel/flask_app.py
+++ devel/jburian_devel/flask_app.py
@@ -1,8 +1,11 @@
 from flask import Flask
 from flask import request, jsonify, render_template
-import sys
+from flask import redirect, url_for
+from flask import send_from_directory
 
-import loguru
+import sys
+from werkzeug.utils import secure_filename
+
 from loguru import logger
 
 from pathlib import Path
@@ -19,11 +22,12 @@ import os
 # predikce s modelem
 # curl -X GET 147.228.140.130:5000/predict?filename="img100.czi"?modelname="mujmodel"
 
-UPLOAD_FOLDER = Path("C:\Temp")
-ALLOWED_EXTENSIONS = {"czi"}
+UPLOAD_FOLDER = "/Temp/Uploaded_files"
+ALLOWED_EXTENSIONS = {".czi"} # povolene formaty
 
 app = Flask(__name__)
 app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+#app.config["MAX_CONTENT_LENGTH"] = 16 * 1000 * 1000 # maximalni velikost souboru
 
 
 def allowed_file(filename):
@@ -62,16 +66,40 @@ def exists():
 @app.route("/upload", methods=["GET", "POST"])
 def upload():
     if request.method == "POST":
-        filename = request.args.get("filename")
+        file = request.files['file']
 
-        if not Path(filename).exists():
-            logger.debug(f"File does not exist. filename={filename}")
-            return jsonify({"error": "File does not exists."})
+        if file.filename == "":
+            logger.debug(f"No file selected.")
+            return jsonify({"error": "No file selected."})
 
-        if not (allowed_file(filename)):
-            logger.debug(f"Uploaded file is in bad format. filename={filename}")
-            return jsonify({"error": "Uploaded file is in bad format."})
+        if not Path(file.filename).exists():
+            logger.debug(f"File does not exist. filename={file.filename}")
+            return jsonify({"error": "File does not exist."})
 
+        if not allowed_file(file.filename):
+            logger.debug(f"Bad format of the file. filename={file.filename}")
+            return jsonify({"error": "Bad format of the file."})
+
+        if file:
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            logger.debug(f"Everything ok. filename={filename}")
+            #return redirect(url_for('download_file', name=filename))
+            return jsonify({"OK-200"})
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''
+
+
+@app.route('/uploads/<name>')
+def download_file(name):
+    return send_from_directory(app.config["UPLOAD_FOLDER"], name)
 
 @app.route("/train", methods=["GET", "POST"])
 def train():
