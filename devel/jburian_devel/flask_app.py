81c700bda49df923c6a5d5d954b74f1ae5fdf616
autoblack <mjirik@users.noreply.github.com>
2022-03-21 14:27:00 +0000

    fixup: Format Python code with Black

--- devel/jburian_devel/flask_app.py
+++ devel/jburian_devel/flask_app.py
@@ -41,37 +41,41 @@ app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 def allowed_file(filename):
     return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
 
+
 @app.route("/hello")
 def hello_world():
     return "<p>Hello, World!</p>"
 
+
 # Set the secret key to some random bytes. Keep this really secret!
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
-@app.route('/')
-def index():
-    if 'username' in session:
-        return f'Logged in as {session["username"]}'
-    return 'You are not logged in'
 
-@app.route('/login', methods=['GET', 'POST'])
+@app.route("/")
+def index():
+    if "username" in session:
+        return f'Logged in as {session["username"]}'
+    return "You are not logged in"
+
+
+@app.route("/login", methods=["GET", "POST"])
 def login():
-    if request.method == 'POST':
-        session['username'] = request.form['username']
-        return redirect(url_for('index'))
-    return '''
+    if request.method == "POST":
+        session["username"] = request.form["username"]
+        return redirect(url_for("index"))
+    return """
         <form method="post">
             <p><input type=text name=username>
             <p><input type=submit value=Login>
         </form>
-    '''
+    """
 
-@app.route('/logout')
+
+@app.route("/logout")
 def logout():
     # remove the username from the session if it's there
-    session.pop('username', None)
-    return redirect(url_for('index'))
-
+    session.pop("username", None)
+    return redirect(url_for("index"))
 
 
 def export_czi_to_jpg(czi_input_path, jpg_output_path, annotation_name):
